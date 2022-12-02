4267518a5a21089160ea25d1403404bc7c88a9e3
Takkapi <72162635+Takkapi@users.noreply.github.com>
2022-12-01 23:13:03 -0800

    Add files via upload

--- Sarcina-Informatica-30-11-2022/Sarcina-Informatica-30-11-2022/menu.cpp
+++ Sarcina-Informatica-30-11-2022/Sarcina-Informatica-30-11-2022/menu.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
+#include <Windows.h>
+#include <conio.h>
 
 #include "menu.h"
 #include "ParImpar.h"
@@ -7,11 +9,81 @@
 
 using namespace std;
 
+void color(int color) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void gotoxy(int x, int y) {
+	COORD c;
+	c.X = x;
+	c.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
 void menu() {
-	int action;
 
 	cout << "Program elaborat de Smolenschi Petru(Takkapi)" << endl;
 	cout << endl;
+	
+	int Set[] = { 7, 7, 7 };
+	int counter = 2;
+	char key;
+
+	for (int i = 0;;) {
+
+		gotoxy(10, 5);
+		color(Set[0]);
+		cout << "[1.] Program Numere Pare/Impare";
+
+		gotoxy(10, 6);
+		color(Set[1]);
+		cout << "[2.] Program Cifre intr-un numar";
+
+		gotoxy(10, 7);
+		color(Set[2]);
+		cout << "[3.] Iesi";
+
+		key = _getch();
+
+		if (key == 72 && (counter >= 2 && counter <= 3)) {
+			counter--;
+		} 
+		if (key == 80 && (counter >= 1 && counter <= 2)) {
+			counter++;
+		}
+		if (key == '\r') {
+			if (counter == 1) {
+				system("cls");
+				ParImpar();
+			}
+			if (counter == 2) {
+				system("cls");
+				CifreInNumar();
+			}
+			if (counter == 3) {
+				cin.ignore();
+				cout << "\nApasa tasta Enter pentru a continua... ";
+				cin.get();
+				exit(0);
+			}
+		}
+		Set[0] = 7;
+		Set[1] = 7;
+		Set[2] = 7;
+		if (counter == 1) {
+			Set[0] = 12;
+		}
+		if (counter == 2) {
+			Set[1] = 12;
+		}
+		if (counter == 3) {
+			Set[2] = 12;
+		}
+	}
+
+	/*
+	int action;
+
 
 	cout << "Tasteaza numarul actiunii" << endl;
 	cout << "[1.] Program Numere Pare/Impare" << endl;
@@ -37,5 +109,5 @@ void menu() {
 	else {
 		cout << "\nID actiune invalid!" << endl;
 		menu();
-	}
+	}*/
 }
